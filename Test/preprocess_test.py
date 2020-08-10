6e03a4975b3af73a1d568d327e502e47aaeba9c3
Maoz-Grossman <44970530+maoz-grossman@users.noreply.github.com>
2020-08-11 00:31:17 +0300

    Uploading Test "about a girl" :musical_note:

---
+++ Test/preprocess_test.py
@@ -0,0 +1,74 @@
+import json
+import os
+import math
+from pitch_class_profiling import PitchClassProfiler
+
+DATASET_PATH = "TheData/where"
+JSON_PATH = "test.json"
+
+def save_pitch(dataset_path, json_path):
+    """Extracts pitch class from music dataset and saves them into a json file along witgh genre labels.
+        :param dataset_path (str): Path to dataset
+        :param json_path (str): Path to json file used to save pitchs
+        :return:
+        """
+
+    # dictionary to store mapping, labels, and pitch classes
+    data = {
+        "mapping": [],
+        "labels": [],
+        "pitch": [],
+        "order":[]
+    }
+
+    # loop through all chord sub-folder
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # ensure we're processing a sub-folder level
+        if dirpath is not dataset_path:
+
+            # save chord label (i.e., sub-folder name) in the mapping
+            semantic_label = dirpath.split("/")[-1]
+            data["mapping"].append(semantic_label)
+            print("\nProcessing: {}".format(semantic_label))
+
+            # process all audio files in chord sub-dir
+            for f in filenames:
+
+                # load audio file
+                file_path = os.path.join(dirpath, f)
+                file_name= file_path.split("/")[-1]
+                file_name2=file_name.split(".")[0]
+                data["order"].append(file_name2)
+                # process all segments of audio file
+
+                ptc=PitchClassProfiler(file_path)
+                data["pitch"].append(ptc.get_profile())
+                data["labels"].append(i - 1)
+                print("{}, segment:{}".format(file_path, 1))
+        else:
+            print("oups")
+    n = len(data["order"]) 
+    for i in range(n-1): 
+    # range(n) also work but outer loop will repeat one time more than needed. 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+  
+            # traverse the array from 0 to n-i-1 
+            # Swap if the element found is greater 
+            # than the next element 
+            if int(data["order"][j]) > int(data["order"][j+1]) : 
+                data["order"][j],data["order"][j+1]=data["order"][j+1],data["order"][j]
+                data["pitch"][j],data["pitch"][j+1]=data["pitch"][j+1],data["pitch"][j]
+                data["labels"][j],data["labels"][j+1]=data["labels"][j+1],data["labels"][j]
+
+
+    # save pitch classes to json file
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+
+
+if __name__ == "__main__":
+    save_pitch(DATASET_PATH, JSON_PATH)
---
+++ Test/test.py
@@ -0,0 +1,64 @@
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+DATA_PATH = "data.json"
+DATA_PATH2="test.json"
+
+def load_data(data_path):
+    """Loads training dataset from json file.
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
+        data = json.load(fp)
+
+    X = np.array(data["pitch"])
+    y = np.array(data["labels"])
+    z=np.array(data["mapping"])
+    return X, y,z
+# get train, validation, test splits
+X, y,z = load_data(DATA_PATH)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0)
+
+#Create a knn Classifier
+model1 = KNeighborsClassifier(n_neighbors=5)
+model2=AdaBoostClassifier(n_estimators=200,learning_rate=2)
+model3=DecisionTreeClassifier()
+model4=SVC(kernel='linear')
+
+#Train the model using the training sets
+model1.fit(X, y)
+model2.fit(X, y)
+model3.fit(X, y)
+model4.fit(X, y)
+
+
+X_test, y_test, z_test = load_data(DATA_PATH2)
+#Predict the response for test dataset
+y_pred1 = model1.predict(X_test)
+y_pred2 = model2.predict(X_test)
+y_pred3 = model3.predict(X_test)
+y_pred4 = model4.predict(X_test)
+#prediction of where_did:
+print("KNN: ")
+for i in range(len(X_test)):
+    print(z[y_pred1[i]],end=' ' )
+print("\nAdaboost: ")
+for i in range(len(X_test)):
+    print(z[y_pred2[i]],end=' ' )
+print("\nDecision tree: ")
+for i in range(len(X_test)):
+    print(z[y_pred3[i]],end=' ' )
+print("\nSVM: ")
+for i in range(len(X_test)):
+    print(z[y_pred4[i]],end=' ' )
+print("\nReal chords: ")
+print("em g em g em em em g em em g g em g g g em g g g em em g em em g g em em g g")
