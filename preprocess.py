5a6048de8a316a418740e69dc17cbd989196f5af
maoz-grossman <maozg2008@gmail.com>
2020-08-10 19:04:07 +0300

    Update preprocess :musical_note:

--- preprocess.py
+++ preprocess.py
@@ -1,78 +1,53 @@
 import json
 import os
 import math
-import librosa
 from pitch_class_profiling import PitchClassProfiler
-DATASET_PATH = "jim2012Chords/Guitar_Only"
-JSON_PATH = "data.json"
-SAMPLE_RATE = 22050
-TRACK_DURATION = 1 # measured in seconds
-SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
+DATASET_PATH = "jim2012Chords\\Guitar_Only"
+JSON_PATH = "data2.json"
 
-def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=2):
-    """Extracts MFCCs from music dataset and saves them into a json file along witgh genre labels.
+def save_pitch(dataset_path, json_path):
+    """Extracts pitch class from music dataset and saves them into a json file along witgh genre labels.
         :param dataset_path (str): Path to dataset
-        :param json_path (str): Path to json file used to save MFCCs
-        :param num_mfcc (int): Number of coefficients to extract
-        :param n_fft (int): Interval we consider to apply FFT. Measured in # of samples
-        :param hop_length (int): Sliding window for FFT. Measured in # of samples
-        :param: num_segments (int): Number of segments we want to divide sample tracks into
+        :param json_path (str): Path to json file used to save pitchs
         :return:
         """
 
-    # dictionary to store mapping, labels, and MFCCs
+    # dictionary to store mapping, labels, and pitch classes
     data = {
         "mapping": [],
         "labels": [],
         "pitch": []
     }
 
-    samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
-    num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
-
-    # loop through all genre sub-folder
+    # loop through all chord sub-folder
     for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
 
-        # ensure we're processing a genre sub-folder level
+        # ensure we're processing a sub-folder level
         if dirpath is not dataset_path:
 
-            # save genre label (i.e., sub-folder name) in the mapping
-            semantic_label = dirpath.split("/")[-1]
+            # save chord label (i.e., sub-folder name) in the mapping
+            semantic_label = dirpath.split("\\")[-1]
             data["mapping"].append(semantic_label)
             print("\nProcessing: {}".format(semantic_label))
 
-            # process all audio files in genre sub-dir
+            # process all audio files in chord sub-dir
             for f in filenames:
 
                 # load audio file
                 file_path = os.path.join(dirpath, f)
-                signal, sample_rate = librosa.load(file_path, sr=SAMPLE_RATE)
 
                 # process all segments of audio file
-                for d in range(num_segments):
-                    '''
-                    # calculate start and finish sample for current segment
-                    start = samples_per_segment * d
-                    finish = start + samples_per_segment
 
-                    # extract mfcc
-                    mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc=num_mfcc, n_fft=n_fft,
-                                                hop_length=hop_length)
-                    mfcc = mfcc.T
+                ptc=PitchClassProfiler(file_path)
+                data["pitch"].append(ptc.get_profile())
+                data["labels"].append(i - 1)
+                print("{}, segment:{}".format(file_path, 1))
 
-                    # store only mfcc feature with expected number of vectors
-                    if len(mfcc) == num_mfcc_vectors_per_segment:
-                    '''
-                    ptc=PitchClassProfiler(file_path)
-                    data["pitch"].append(ptc.get_profile())
-                    data["labels"].append(i - 1)
-                    print("{}, segment:{}".format(file_path, d + 1))
-
-    # save MFCCs to json file
+    # save pitch classes to json file
     with open(json_path, "w") as fp:
         json.dump(data, fp, indent=4)
 
 
 if __name__ == "__main__":
-    save_mfcc(DATASET_PATH, JSON_PATH, num_segments=1)
+    save_pitch(DATASET_PATH, JSON_PATH)
